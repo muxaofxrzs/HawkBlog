b2914a06e282a4ba1b015d028a96080eaa0921f5
muxaofxrzs <134760144+muxaofxrzs@users.noreply.github.com>
2023-09-08 10:43:32 +0800

    Delete model directory

--- model/like.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type CommentLike struct {
-	UserId    int64 `json:"user_id"`
-	ArticleId int64 `json:"article_id"`
-	CommentId int64 `json:"comment_id"`
-	Status    int64 `json:"status"`
-}
